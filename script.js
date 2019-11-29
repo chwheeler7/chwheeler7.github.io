79e317a9f714101b776ca9d72ab071f80268243d
Chris Wheeler <chris@iterable.com>
2019-11-29 14:59:33 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -13,17 +13,17 @@ var birthday = document.getElementById("birthday");
 birthday.setAttribute("max", maxDate);
 birthday.setAttribute("min", minDate);
 
-var container = document.querySelector(".container");
-var rgbwindow = document.getElementById("rgb");
+// var container = document.querySelector(".container");
+// var rgbwindow = document.getElementById("rgb");
 
-container.addEventListener("click", changeColor)
+// container.addEventListener("click", changeColor)
 
-function changeColor(event) {
-	var red = Math.floor(Math.random() * 256);
-	var green = Math.floor(Math.random() * 256);
-	var blue = Math.floor(Math.random() * 256);
-	var rgba = "rgba("+red+", "+green+", "+blue+", 0.95)";
-	var rgba2 = "rgba("+(255-red)+", "+(255-green)+", "+(255-blue)+", 0.95)";
-	container.style.background = "linear-gradient(to bottom, "+rgba+", "+rgba2+")";
-	rgbwindow.textContent = rgba;
-}
+// function changeColor(event) {
+// 	var red = Math.floor(Math.random() * 256);
+// 	var green = Math.floor(Math.random() * 256);
+// 	var blue = Math.floor(Math.random() * 256);
+// 	var rgba = "rgba("+red+", "+green+", "+blue+", 0.95)";
+// 	var rgba2 = "rgba("+(255-red)+", "+(255-green)+", "+(255-blue)+", 0.95)";
+// 	container.style.background = "linear-gradient(to bottom, "+rgba+", "+rgba2+")";
+// 	rgbwindow.textContent = rgba;
+// }
